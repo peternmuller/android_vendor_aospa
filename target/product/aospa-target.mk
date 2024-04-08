05515c936a12b472296b9f5eedb00f4d30407ce5
Richard Raya <rdxzv.dev@gmail.com>
2024-04-07 21:42:48 -0300

    target: Optimize Settings too
    Change-Id: I67ed6b4942fdf3a5e15d3b16b895ce9e0ef7f8d8
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -56,7 +56,8 @@ DONT_DEXPREOPT_PREBUILTS := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
-    ParanoidSystemUI
+    ParanoidSystemUI \
+    Settings
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.systemuicompilerfilter=speed
