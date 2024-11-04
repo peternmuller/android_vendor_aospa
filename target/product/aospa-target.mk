15d1e8b776a21b1abd4e2edbdfa25e6dc2191a98
LuK1337 <priv.luk@gmail.com>
2024-11-04 20:24:31 +0100

    config: Add text classifier files to allowed product artifacts
    Change-Id: Ia568e8ad8aec14784f1649ec4e14b961a3c17f91

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -255,6 +255,12 @@ PRODUCT_PACKAGES += \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/textclassifier/actions_suggestions.universal.model \
+    system/etc/textclassifier/lang_id.model \
+    system/etc/textclassifier/textclassifier.en.model \
+    system/etc/textclassifier/textclassifier.universal.model
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwpa_client
