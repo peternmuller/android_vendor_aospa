363faabe0db7d7e27ebc832631277adf87745043
Fazil Sheik <fazil.sheik96@gmail.com>
2024-11-30 12:20:10 +0530

    aospa: Build with ThinLTO Cache
    Change-Id: Ie184b05614747672ea371b818d903d55c890e5a4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,6 @@
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
+
+#  Build with ThinLTO Cache
+export USE_THINLTO_CACHE=true
