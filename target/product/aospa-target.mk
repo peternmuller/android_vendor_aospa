14fc185973a6e8cf20d7ce639128699aabc72958
peternmuller <pm563838@gmail.com>
2025-08-26 01:28:29 +0200

    aospa-target: Set speed-profile as default compiler filter
    * Also always preopt extracted APKs to prevent extracting out of the APK for gms modules
    
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     curl
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Use a generic profile based boot image by default
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
